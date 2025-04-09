7889b6c6eeb6ca87ffeb0dd8e0902cf2ae3a484e
Julian Millen <julimillen@gmail.com>
2025-04-09 12:12:06 -0300

    agregando componentes

--- cliente/src/app/app.routes.ts
+++ cliente/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { EditarProductoComponent } from './components/editar-producto/editar-producto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: ListarProductosComponent},
+    { path: 'crear-producto', component: CrearProductoComponent },
+    { path:'editar-producto/:id', component:EditarProductoComponent},
+    { path: '**', redirectTo:'', pathMatch:'full' }
+];
+
