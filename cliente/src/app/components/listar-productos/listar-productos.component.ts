660f26a30f5aa67dad100fc193aeb69f882a35e1
Julian Millen <julimillen@gmail.com>
2025-04-10 18:29:15 -0300

    agregando fuentes y estilos

--- cliente/src/app/components/listar-productos/listar-productos.component.ts
+++ cliente/src/app/components/listar-productos/listar-productos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-listar-productos',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './listar-productos.component.html',
   styleUrl: './listar-productos.component.css'
 })
