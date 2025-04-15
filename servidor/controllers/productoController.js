459bdc6a5a2388e5a77899f77e03ded045292b16
Julian Millen <julimillen@gmail.com>
2025-04-15 18:09:49 -0300

    agregando productos

--- servidor/controllers/productoController.js
+++ servidor/controllers/productoController.js
@@ -1,3 +1,19 @@
-exports.crearProducto = (req,res) =>{
-    console.log('desde crear producto');
+const Producto = require("../models/Producto");
+
+exports.crearProducto = async (req,res) =>{
+    try{
+
+        let producto;
+
+        producto = new Producto(req.body);
+
+        await producto.save();
+        res.send(producto);
+
+
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Hubo un error");
+    }
 }
--- servidor/index.js
+++ servidor/index.js
@@ -6,14 +6,12 @@ const app = express();
 //conectar a la BD
 conectarDB();
 
+app.use(express.json());
+
+
 app.use('/api/productos', require('./routes/producto'));
 
 
-//definir ruta principal
-
-app.get('/', (req,res) =>{
-    res.send('Hola Mundo')
-})
 
 app.listen(4000, () => {
     console.log('El servidor esta corriendo correctamente');
