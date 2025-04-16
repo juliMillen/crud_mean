eff04c05e15d92dfcd7cfd281faa5cc447e7add9
Julian Millen <julimillen@gmail.com>
2025-04-16 12:55:42 -0300

    completando el crud de productos

--- servidor/controllers/productoController.js
+++ servidor/controllers/productoController.js
@@ -17,3 +17,66 @@ exports.crearProducto = async (req,res) =>{
         res.status(500).send("Hubo un error");
     }
 }
+
+
+exports.obtenerProductos = async (req,res) => {
+    try{
+        const productos = await Producto.find();
+        res.json(productos);
+    }catch(error){
+        console.error(error)
+        res.status(500).send("Hubo un error");
+    }
+}
+
+exports.actualizarProducto = async (req,res) => {
+    try{
+        const { nombre, categoria , ubicacion, precio} = req.body;
+
+        let producto = await Producto.findById(req.params.id);
+        if(!producto){
+            res.status(404).json({msg: 'El producto no existe'})
+        }
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+        producto = await Producto.findOneAndUpdate({_id:req.params.id},producto,{new: true});
+        res.json(producto);
+    }catch(error){
+        console.log(error);
+        req.status(500).send("Hubo un error");
+    }
+}
+
+
+exports.obtenerProducto = async (req,res) => {
+    try{
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            res.status(404).json({msg: 'No existe el producto'})
+        }
+        res.json(producto);
+
+    }catch(error){
+        console.log(error);
+        req.status(500).send("Error al obtener el producto");
+    }
+}
+
+exports.deleteProducto = async (req,res) => {
+    try{
+        let aEliminar = await Producto.findById(req.params.id);
+
+        if(!aEliminar){
+            res.status(404).json({msg:'No existe el producto'})
+        }
+
+        await Producto.findOneAndRemove({_id:req.params.id})
+        res.json({msg:'Producto eliminado correctamente'})
+    }catch(error){
+        console.log(error);
+        req.status(500).send("Error al intentar eliminar el producto");
+    }
+}
--- servidor/routes/producto.js
+++ servidor/routes/producto.js
@@ -4,5 +4,9 @@ const productoController = require('../controllers/productoController');
 
 //api/productos
 router.post('/', productoController.crearProducto);
+router.get('/',productoController.obtenerProductos);
+router.get('/:id',productoController.obtenerProducto);
+router.put('/:id',productoController.obtenerProducto);
+router.delete('/:id',productoController.deleteProducto);
 
 module.exports = router;
